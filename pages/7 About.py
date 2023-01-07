0d44d9da335a1b3feaa9e9b7f789e71d6364b69f
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-01-07 23:37:25 +0100

    Update 7 About.py

--- pages/7 About.py
+++ pages/7 About.py
@@ -21,7 +21,7 @@ st.title('About')
 st.markdown('This app has been done by **_Adrià Parcerisas_**, a PhD Biomedical Engineer related to Machine Learning and Artificial intelligence technical projects for data analysis and research, as well as dive deep on-chain data analysis about cryptocurrency projects. You can find me on [Twitter](https://twitter.com/adriaparcerisas)')
 st.write('')
 
-st.markdown('The full sources used to develop this app can be found to the following link: [Github link](https://github.com/adriaparcerisas/SolanaNFT-app)')
+st.markdown('The full sources used to develop this app can be found to the following link: [Github link](https://github.com/adriaparcerisas/Solana-NFT-in-2023)')
 
 st.markdown('_Powered by [Flipside Crypto](https://flipsidecrypto.xyz) and [MetricsDAO](https://metricsdao.notion.site)_')
 
